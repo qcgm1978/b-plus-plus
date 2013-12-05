bcbdc0d47a5ecb98c6c2e27f6fa1f942188434f8
qcgm1978 <zhanghongliang@raxtone.com>
2013-12-05 17:16:23 +0800

    commit

---
+++ temp.js
@@ -0,0 +1,13 @@
+/**
+ * Created with WebStorm.
+ * User: qcgm1978
+ */
+test( "equal test", function() {
+  equal( 0, 0, "Zero; equal succeeds" );
+  equal( "", 0, "Empty, Zero; equal succeeds" );
+  equal( "", "", "Empty, Empty; equal succeeds" );
+  equal( 0, 0, "Zero, Zero; equal succeeds" );
+
+  equal( "three", 3, "Three, 3; equal fails" );
+  equal( null, false, "null, false; equal fails" );
+});
