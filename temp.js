c746044c0400520518f61472509d030f16f1ac61
qcgm1978 <zhanghongliang@raxtone.com>
2013-12-10 14:43:18 +0800

    jasmine test added

--- temp.js
+++ temp.js
@@ -1,41 +1,31 @@
-readme_generator= {
-    my_generator : { //Thank you @JFusco
-      options: {
-        // Task-specific options go here.
-        // detailed explanation is under options
-        // Default options:
-        readme_folder: "readme",
-        output: "README.md",
-        table_of_contents: true,
-        toc_extra_links: [],
-        generate_changelog: false,
-        changelog_folder: "changelogs",
-        changelog_version_prefix: null,
-        changelog_insert_before: null,
-        banner: null,
-        has_travis: true,
-        github_username: "aponxi",
-        travis_branch: "master",
-        generate_footer: true,
-        generate_title: true,
-        package_title: null,
-        package_name: null,
-        package_desc: null,
-        informative: true,
-        h1: "#",
-        h2: "##",
-        back_to_top_custom: null
+describe("A spy, when faking a return value", function() {
+  var foo, bar, fetchedBar;
+
+  beforeEach(function() {
+    foo = {
+      setBar: function(value) {
+        bar = value;
       },
-      order: {
-        // Title of the piece and the File name goes here
-        // "Filename" : "Title"
-        "installation.md": "Installation",
-        "usage.md": "Usage",
-        "options.md": "Options",
-        "example.md": "Example",
-        "output.md": "Example Output",
-        "building-and-testing.md": "Building and Testing",
-        "legal.md": "Legal Mambo Jambo"
+      getBar: function() {
+        return bar;
       }
-    }
-  }
+    };
+
+    spyOn(foo, 'getBar').andReturn(745);
+
+    foo.setBar(123);
+    fetchedBar = foo.getBar();
+  });
+
+  it("tracks that the spy was called", function() {
+    expect(foo.getBar).toHaveBeenCalled();
+  });
+
+  it("should not affect other functions", function() {
+    expect(bar).toEqual(123);
+  });
+
+  it("when called returns the requested value", function() {
+    expect(fetchedBar).toEqual(745);
+  });
+});
